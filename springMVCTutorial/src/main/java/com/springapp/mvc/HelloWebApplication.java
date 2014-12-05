a2b491769970dd7e0974f3582333bd94d66957fc
Mallika Kulkarni <mallikak2709@gmail.com>
2014-12-05 11:32:07 -0800

    Mallika's local copy

---
+++ springMVCTutorial/src/main/java/com/springapp/mvc/HelloWebApplication.java
@@ -0,0 +1,17 @@
+package com.springapp.mvc;
+
+import org.springframework.boot.SpringApplication
+/**
+ * This object bootstraps Spring Boot web application.
+ * Via Gradle: gradle bootRun
+ *
+ * @author saung
+ * @since 1.0
+ */
+
+public class HelloWebApplication {
+
+	public static void main(String[] args) {
+	   SpringApplication.run(classOf[HelloController]);
+	}
+}
